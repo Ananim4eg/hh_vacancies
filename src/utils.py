4bbd65cef5e1301ba250a9285424f26ee301a828
Ananim4eg <109145711+Ananim4eg@users.noreply.github.com>
2025-09-04 16:12:23 +0300

    edit classes

---
+++ src/utils.py
@@ -0,0 +1,13 @@
+from typing import Any
+
+
+def get_top_vacancies_by_salary(list_vacancies: [dict[str, Any]], top_n: int) -> list[dict[str, Any]]:
+    ...
+
+
+def sort_vacancies():
+    ...
+
+
+def filter_vacancies():
+    ...
